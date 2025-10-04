9839ccda056c6a615d95dabeec974691d40cf0f2
fauzanputra028 <akunyttpremium028@gmail.com>
2025-10-04 23:53:41 +0700

    Add files via upload

---
+++ src/js/join-us.js
@@ -0,0 +1,46 @@
+//Gfroms
+const iframe = document.getElementById('gform');
+      iframe.onload = function () {
+        try {
+          // ambil tinggi konten di dalam iframe
+          const iframeDoc = iframe.contentWindow.document.documentElement;
+          iframe.style.height = iframeDoc.scrollHeight + 'px';
+        } catch (e) {
+          // fallback kalau cross-origin (Google Forms tidak selalu izinkan)
+          iframe.style.height = "1000px";
+        }
+      };
+
+// Text Berjalan
+const textEl = document.getElementById('text')
+const texts = [
+    'Official Fanbase JKT48 Regional Pekanbaru',
+    'Ayo Bergabung Bersama Kami!',
+    'Dukung JKT48 Bersama!'
+]
+
+let idx = 1
+let textIndex = 0
+let speed = 180
+
+function writeText() {
+    const currentText = texts[textIndex] 
+    textEl.innerText = currentText.slice(0, idx)
+
+    idx++
+
+    if (idx > currentText.length) {
+        idx = 1
+        textIndex++
+        if (textIndex >= texts.length) {
+            textIndex = 0
+        }
+    }
+
+    setTimeout(writeText, speed)
+}
+
+writeText()
+
+// Year
+document.getElementById('year').textContent = new Date().getFullYear();
---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+// Text Berjalan
+const textEl = document.getElementById('text')
+const texts = [
+    'Official Fanbase JKT48 Regional Pekanbaru',
+    'Ayo Bergabung Bersama Kami!',
+    'Dukung JKT48 Bersama!'
+]
+
+let idx = 1
+let textIndex = 0
+let speed = 180
+
+function writeText() {
+    const currentText = texts[textIndex] 
+    textEl.innerText = currentText.slice(0, idx)
+
+    idx++
+
+    if (idx > currentText.length) {
+        idx = 1
+        textIndex++
+        if (textIndex >= texts.length) {
+            textIndex = 0
+        }
+    }
+
+    setTimeout(writeText, speed)
+}
+
+writeText()
+
+
+//Warning
+document.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+
+        // Disable F12
+        document.addEventListener('keydown', function (e) {
+            if (e.keyCode === 123) { // F12
+                e.preventDefault();
+            }
+        });
+
+// Year
+document.getElementById('year').textContent = new Date().getFullYear();
+
